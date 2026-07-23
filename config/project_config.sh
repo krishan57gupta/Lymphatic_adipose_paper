60dc8b0066831b4a0356778be372135213da98ba
Krishan Gupta <26205662+krishan57gupta@users.noreply.github.com>
2026-07-23 19:34:06 -0400

    Update project_config.sh

--- config/project_config.sh
+++ config/project_config.sh
@@ -2,4 +2,3 @@
 export FOXC2_PROJECT_ROOT="/absolute/path/to/Lymphatic_adipose_paper"
 export CELLRANGER_TRANSCRIPTOME="/absolute/path/to/cellranger/reference"
 export VELOCYTO_GTF="/absolute/path/to/genes.gtf"
-export VELOCYTO_MASK_GTF=""
